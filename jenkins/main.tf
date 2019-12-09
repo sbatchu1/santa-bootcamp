72edf6830f7d74bc758f871b385b7641d82fc8b7
tarun.gupta <tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com>
2019-12-09 15:43:36 +0530

    tarun-terraform-code

---
+++ jenkins/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket  = "santa-s3-bucket"
+    key     = "team-santa"
+    region  = "ap-south-1"
+    profile = "ShellPowerUser"
+  }
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "ShellPowerUser"
+}
+
+variable "ami" {
+  type        = string
+  default     = "ami_ID"
+  description = "ami_ID"
+}
+module "pt_master_module" {
+  source = "./module/master"
+  # var.ami = "ami-0ca4ee4403ecb3a4e"
+}
+
+# module "pt_node_module" {
+#    source = "./module/node"
+  #  var.ami = "ami-0612808b4a0ca35a1"
+#  }
---
+++ jenkins/module/master/master_instance.tf
@@ -0,0 +1,76 @@
+
+resource "aws_instance" "pt_master_module" {
+  ami                    = "ami-03ea53d04854b1927"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.sg_jenkins.id}"]
+  # aws_security_group = "${aws_security_group.sg_jenkins.name}"
+  key_name = "master-key"
+  tags = {
+    Name = "Jenkins Master_Santa"
+  }
+}
+
+resource "aws_security_group" "sg_jenkins" {
+  name        = "sg2"
+  description = "Allows all traffic"
+
+  # SSH
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+
+# HTTP
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+  # Jenkins JNLP port
+  ingress {
+    from_port = 50000
+    to_port   = 50000
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+}
+
+
+output "instance_id" {
+  value = "${aws_instance.pt_master_module.id}"
+}
