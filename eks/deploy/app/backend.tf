5ac5df016ff6fc43700e02ae9ad8ae6fd102cbc7
tarun.gupta <tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com>
2019-12-10 16:48:28 +0530

    added mike in the state

--- eks/deploy/app/backend.tf
+++ eks/deploy/app/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "santa-s3-bucket"
-    key = "k8s/terraform.tfstate"
+    key = "k8s-mike/terraform.tfstate"
     region = "ap-south-1"
     profile = "ShellPowerUser"
   }
