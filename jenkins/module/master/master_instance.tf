0809b4787698bdd9dc812e6c172a694188adc95a
sairamgopal.batchu <sairamgopal.batchu@sairamgopalbatchus-MacBook-Pro.local>
2019-12-11 15:07:58 +0530

    updated role tf

--- jenkins/module/master/master_instance.tf
+++ jenkins/module/master/master_instance.tf
@@ -83,5 +83,195 @@ output "instance_id" {
 # defining user profile for the server
 resource "aws_iam_instance_profile" "test_profile" {
   name = "santa_profile"
-  role = "ShellPowerUser"
+  role = "SantaPowerUser"
+}
+
+
+resource "aws_iam_role_policy" "eks_policy" {
+  name = "eks_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "EKSPolicy",
+            "Effect": "Allow",
+            "Action": "eks:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "s3_policy" {
+  name = "s3_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Deny",
+            "Action": "s3:delete*",
+            "Resource": [
+                "arn:aws:s3:::terraform-trainers-remote-state/*",
+                "arn:aws:s3:::terraform-trainers-remote-state"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:*",
+                "autoscaling:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Deny",
+            "Action": "ec2:*",
+            "Resource": "*",
+            "Condition": {
+                "StringNotEquals": {
+                    "ec2:InstanceType": [
+                        "t2.micro",
+                        "t3.medium"
+                    ],
+                    "ec2:Region": "ap-south-1"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "elb_policy" {
+  name = "elb_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "role_policy" {
+  name = "role_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateInstanceProfile",
+                "iam:DeleteInstanceProfile",
+                "iam:UpdateAssumeRolePolicy",
+                "iam:List*",
+                "iam:RemoveRoleFromInstanceProfile",
+                "iam:DeletePolicy",
+                "iam:CreateRole",
+                "iam:DeleteRole",
+                "iam:AttachRolePolicy",
+                "iam:*Tag*",
+                "iam:AddRoleToInstanceProfile",
+                "iam:CreateAccessKey",
+                "iam:CreatePolicy",
+                "iam:PassRole",
+                "iam:Get*",
+                "iam:DetachRolePolicy",
+                "iam:DeleteRolePolicy",
+                "iam:PutRolePolicy",
+                "iam:UpdateRole"
+            ],
+            "Resource": "*"
+
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "ecr_policy" {
+  name = "ecr_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "ecr:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role" "test_role" {
+  name = "SantaPowerUser"
+
+  assume_role_policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+EOF
+
+  tags = {
+    tag-key = "SantaPowerUser"
+  }
 }
