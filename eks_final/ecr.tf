0c138dfc0fb6a7a5999710cfa52d6af64d58b8ab
tarun.gupta <tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com>
2019-12-12 11:44:52 +0530

    added jenkins file for eks and copied ecr file there

---
+++ eks_final/ecr.tf
@@ -0,0 +1,52 @@
+
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = "ap-south-1"
+  profile = "ShellPowerUser"
+}
+
+resource "aws_ecr_repository" "webapp" {
+  name                 = "santa/webapp"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
+resource "aws_ecr_repository" "apigateway" {
+  name                 = "santa/apigateway"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "position-simulator" {
+  name                 = "santa/position-simulator"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "position-tracker" {
+  name                 = "santa/position-tracker"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "queue" {
+  name                 = "santa/queue"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
